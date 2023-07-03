e90072bcfc32514a5a0ed1a0395b35574dd04b87
Sun Xufei <xufei.sun@sap.com>
2023-07-03 11:26:07 +0800

    dev

---
+++ src/DbFieldType.ts
@@ -0,0 +1,9 @@
+export enum DbFieldType {
+    UUID = "uuid",
+    STRING =  "string",
+    TEXT = "text",
+    NUMBER = "number",
+    DECIMAL = "decimal",
+    DATE = "date",
+    TIME = "time"
+}
